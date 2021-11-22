1f1f69ed0b5cfe144c30fc3aee7f9d24a9804afe
Laraibi <meh.laraibi@gmail.com>
2021-11-22 23:05:45 +0100

    to day results

--- toDayResults.py
+++ toDayResults.py
@@ -1,6 +1,7 @@
 # import requests
 # from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common import by
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -23,61 +24,72 @@ try:
 except TimeoutException:
     driver.quit()
 
-
 matchsList = []
-eventsElements = driver.find_elements(
-    By.CSS_SELECTOR, "div.event__match--twoLine,div.event__header")
-# eventsElements = driver.find_elements(
-#     By.XPATH, "//div[@class='event__match--twoLine' or @class='event__header']")
-# print(len(eventsElements))
-day = driver.find_element(
-    By.CSS_SELECTOR, "div.calendar__datepicker").text.split(" ")[0]
-for x in range(len(eventsElements)):
-# for x in range(20):
-    event = eventsElements[x]
-    if "event__header" in event.get_attribute("class").split(" "):
-        league = event.find_element(By.CLASS_NAME, "event__title--name").text
-        country = event.find_element(
-            By.CLASS_NAME, "event__title--type").text.replace("\"", "")
-        # print(league)
-        continue
-    else:
-        # print("no league")
-        match = event
-    # print(match.text)
-    home = match.find_elements(By.CLASS_NAME, "event__participant--home")[
-        0].text if len(match.find_elements(By.CLASS_NAME, "event__participant--home")) > 0 else ""
-    away = match.find_elements(By.CLASS_NAME, "event__participant--away")[
-        0].text if len(match.find_elements(By.CLASS_NAME, "event__participant--away")) > 0 else ""
-    time = match.find_elements(By.CLASS_NAME, "event__time")[0].text if len(match.find_elements(
-        By.CLASS_NAME, "event__time")) > 0 else ""
-    score = {
-        "home": 0,
-        "away": 0,
-    }
-    score["away"] = match.find_elements(By.CLASS_NAME, "event__score--away")[0].text if len(match.find_elements(
-        By.CLASS_NAME, "event__score--away")) > 0 else ""
-    score["home"] = match.find_elements(By.CLASS_NAME, "event__score--home")[0].text if len(match.find_elements(
-        By.CLASS_NAME, "event__score--home")) > 0 else ""
-    # league = match.find_elements(
-    #     By.XPATH, "//div[@class='event__header'][1]")[0].text
-    # league = match.find_elements(
-    #     By.XPATH, "//preceding::div[@class='event__header']")[0].text
-    # print(league)
-    isEnded = "unKnown"
-    if len(match.find_elements(By.CLASS_NAME, "event__stage--block")) > 0:
-        isEnded = True if match.find_element(
-            By.CLASS_NAME, "event__stage--block").text == "Terminé" else False
 
-    matchsList.append({"home": home,
-                       "away": away,
-                       "time": time,
-                       "league": league,
-                       "country": country,
-                       "score": score,
-                       "Day": day,
-                       "Ended": isEnded
-                       })
+for x in range(1):
+        
+    driver.find_element(By.CLASS_NAME,"calendar__direction--tomorrow").click()
+
+    timeout = 90
+    try:
+        WebDriverWait(driver, timeout).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "event__match")))
+    except TimeoutException:
+        driver.quit()
+
+    eventsElements = driver.find_elements(
+        By.CSS_SELECTOR, "div.event__match--twoLine,div.event__header")
+    # eventsElements = driver.find_elements(
+    #     By.XPATH, "//div[@class='event__match--twoLine' or @class='event__header']")
+    # print(len(eventsElements))
+    day = driver.find_element(
+        By.CSS_SELECTOR, "div.calendar__datepicker").text.split(" ")[0]
+    for x in range(len(eventsElements)):
+    # for x in range(20):
+        event = eventsElements[x]
+        if "event__header" in event.get_attribute("class").split(" "):
+            league = event.find_element(By.CLASS_NAME, "event__title--name").text
+            country = event.find_element(
+                By.CLASS_NAME, "event__title--type").text.replace("\"", "")
+            # print(league)
+            continue
+        else:
+            # print("no league")
+            match = event
+        # print(match.text)
+        home = match.find_elements(By.CLASS_NAME, "event__participant--home")[
+            0].text if len(match.find_elements(By.CLASS_NAME, "event__participant--home")) > 0 else ""
+        away = match.find_elements(By.CLASS_NAME, "event__participant--away")[
+            0].text if len(match.find_elements(By.CLASS_NAME, "event__participant--away")) > 0 else ""
+        time = match.find_elements(By.CLASS_NAME, "event__time")[0].text if len(match.find_elements(
+            By.CLASS_NAME, "event__time")) > 0 else ""
+        score = {
+            "home": 0,
+            "away": 0,
+        }
+        score["away"] = match.find_elements(By.CLASS_NAME, "event__score--away")[0].text if len(match.find_elements(
+            By.CLASS_NAME, "event__score--away")) > 0 else ""
+        score["home"] = match.find_elements(By.CLASS_NAME, "event__score--home")[0].text if len(match.find_elements(
+            By.CLASS_NAME, "event__score--home")) > 0 else ""
+        # league = match.find_elements(
+        #     By.XPATH, "//div[@class='event__header'][1]")[0].text
+        # league = match.find_elements(
+        #     By.XPATH, "//preceding::div[@class='event__header']")[0].text
+        # print(league)
+        isEnded = "unKnown"
+        if len(match.find_elements(By.CLASS_NAME, "event__stage--block")) > 0:
+            isEnded = True if match.find_element(
+                By.CLASS_NAME, "event__stage--block").text == "Terminé" else False
+
+        matchsList.append({"home": home,
+                        "away": away,
+                        "time": time,
+                        "league": league,
+                        "country": country,
+                        "score": score,
+                        "Day": day,
+                        "Ended": isEnded
+                        })
 # exit()
 # print(matchsList)
 # jsonStr = json.dumps(matchsList)
