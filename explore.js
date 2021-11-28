4e3192dee3703251d005a3f57e3642b9b9bd3680
Mehdi Laraibi <034836@intelcia.com>
2021-11-28 20:45:00 +0100

    add explore.js

---
+++ explore.js
@@ -0,0 +1,38 @@
+let fs = require("fs");
+jsonFile = fs.readFileSync("matchsToDay.json");
+let matchs = JSON.parse(jsonFile);
+// console.log(matchs.length)
+
+stats = {
+  matchCountByDay: {},
+  matchCountByLeague: {},
+  matchCountByCountry: {},
+};
+
+days = [...new Set(matchs.map((match) => match.Day))];
+leagues = [...new Set(matchs.map((match) => match.league))];
+countries = [...new Set(matchs.map((match) => match.country))];
+
+days.forEach((element) => {
+  stats.matchCountByDay[element] = matchs.filter(
+    (match) => (match.Day == element)
+  ).length;
+});
+leagues.forEach((element) => {
+  stats.matchCountByLeague[element] = matchs.filter(
+    (match) => (match.league == element)
+  ).length;
+});
+countries.forEach((element) => {
+  stats.matchCountByCountry[element] = matchs.filter(
+    (match) => (match.country == element)
+  ).length;
+});
+
+
+// console.log(stats)
+matchs.filter((match)=>match.league=="Bundesliga").forEach((match)=>{
+    console.log("Bundesliga Match")
+    console.log(`Day : ${match.Day}`)
+    console.log(`${match.home} VS ${match.away} `)
+})
